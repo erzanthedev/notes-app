966083908758e56fd2f450d9e7ff2db3c19f1be3
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-27 17:50:00 +1000

    feat: added and setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBitxjExesnGmK8Ng4hjlpKl8ubU_ZF1Vg",
+  authDomain: "react-notes-e79b0.firebaseapp.com",
+  projectId: "react-notes-e79b0",
+  storageBucket: "react-notes-e79b0.appspot.com",
+  messagingSenderId: "211526449896",
+  appId: "1:211526449896:web:d0170e1739148d8594a75d",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const notesCollection = collection(database, "notes");
